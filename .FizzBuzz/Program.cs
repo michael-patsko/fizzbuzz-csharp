0f48990e9ffde90928b8fdb663e5c8c750d62e04
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-05-30 13:35:59 +0100

    prints fizz and buzz on multiples of 3 and 5

--- .FizzBuzz/Program.cs
+++ .FizzBuzz/Program.cs
@@ -5,11 +5,34 @@
         public static void Main(string[] args)
         {
             FizzBuzz fizzBuzz = new FizzBuzz();
+            List<string> outputString = new List<string>();
             int maxNumber = 100;
+            bool isEmpty;
+            int stringLength;
 
             for (int i = 1; i <= maxNumber; i++)
             {
-                Console.WriteLine(i);
+                if (fizzBuzz.IsDivisibleBy(i, 3))
+                {
+                    outputString.Add("Fizz");
+                }
+                if (fizzBuzz.IsDivisibleBy(i, 5))
+                {
+                    outputString.Add("Buzz");
+                }
+
+                stringLength = outputString.Count;
+                isEmpty = (stringLength == 0);
+
+                if (isEmpty)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(string.Join("", outputString));
+                }
+                outputString.Clear();
             }
         }
     }
